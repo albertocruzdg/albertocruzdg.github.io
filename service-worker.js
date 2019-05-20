ec915969342ba55355851f29bec93839badb99c0
albertocruzdg <cruz.alberto92@gmail.com>
2019-05-19 23:39:17 -0300

    Trying modifying and deploying again

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.75bf3d5feac287622ba695b84079b5af.js"
 );
 
 self.addEventListener('message', (event) => {
