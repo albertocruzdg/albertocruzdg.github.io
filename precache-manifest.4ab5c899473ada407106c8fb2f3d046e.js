f22b6b52fc2090b9fef838cad809a191e1bd939e
albertocruzdg <cruz.alberto92@gmail.com>
2019-05-21 23:18:49 -0300

    First deploy

---
+++ precache-manifest.4ab5c899473ada407106c8fb2f3d046e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c11d77d1d937c33cc3325529f2f40a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb866d732aad340cd614",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "53a41026c2d9d4097784",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "eb866d732aad340cd614",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "53a41026c2d9d4097784",
+    "url": "/static/js/main.17d1d609.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
